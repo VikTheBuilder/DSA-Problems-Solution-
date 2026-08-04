8933848b2afe708fcf07f3ae0894a3ae5650b69e
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-08-04 19:53:01 +0530

    Add findMissingElements method in Solution class
    Implement a method to find missing elements in an integer array.

---
+++ Find Missing Elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> result= new ArrayList<>();
+        int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            max=Math.max(max, nums[i]);
+            min=Math.min(min, nums[i]);
+        }
+        for(int j= min; j<max; j++){
+        if(!contains(nums, j)){
+                result.add(j); 
+            }
+        }
+        return result;
+    }
+    boolean contains(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
