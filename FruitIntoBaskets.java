3be7a0e982c7eaacaaf2cf12c2df2a28738ed948
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-08-24 22:14:00 +0530

    Implement totalFruit method for fruit basket problem

--- FruitIntoBaskets.java
+++ FruitIntoBaskets.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int l=0, r=0;
+        int maxL =0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(r<fruits.length){
+            map.put(fruits[r],map.getOrDefault(fruits[r], 0) + 1);
+            if(map.size()>2){
+                map.put(fruits[l],  map.get(fruits[l]) - 1);
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
+                l++;
+            }
+            if(map.size()<=2){
+                maxL = Math.max(maxL, r-l+1);
+            }
+            r++;
+        }
+        return maxL;
+    }
+}
