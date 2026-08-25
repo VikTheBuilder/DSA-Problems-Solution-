e5f5242cd0d2c2b5a9ec6522de73abcd34598022
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-08-25 23:48:52 +0530

    Update SmallestMissingMultipleOfK.java

--- SmallestMissingMultipleOfK.java
+++ SmallestMissingMultipleOfK.java
@@ -1,1 +1,14 @@
+//Solution 1
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        List<Integer> list = Arrays.stream(nums).boxed().toList();
+        int n=1;
+        while(true){
+            if(!list.contains(k*n)){
+                return k*n;
+            }
+            n++;
+        }
+    }
+}
 
