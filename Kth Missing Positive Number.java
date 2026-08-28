db23b041d52deae964f0091ffa1305731edaf13d
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-08-28 19:07:01 +0530

    Implement brute force solution for Kth missing positive

--- Kth Missing Positive Number.java
+++ Kth Missing Positive Number.java
@@ -1,1 +1,26 @@
+//Brute-
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int i = 1, j=1;
+        while(k>0){
+            if(search(arr, i) == -1) k--;
+            i++;
+        }
+        return i-1;
+    }
+    public int search(int[] nums, int target) {
+        int n= nums.length;
+        int low=0; 
+        int high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (nums[mid] == target) return mid;
+            else if (target > nums[mid]) low = mid + 1;
+            else high = mid-1;
+        }
+        return -1;
+    }
+}
+
+//Optimal--
 
