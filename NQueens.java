fa61c120f55a09d9596412e9320184051e49f5e7
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-09-01 21:55:59 +0530

    Implement solve method for N-Queens problem
    Added a recursive solve method to implement the N-Queens solution.

--- NQueens.java
+++ NQueens.java
@@ -9,3 +9,27 @@ class Solution {
         solve(0, board, n, leftRow, upperDiagonal, lowerDiagonal, res);
         return res;
     }
+    public void solve(int col, char[][] board, int n, int[] leftRow, int[] upperDiagonal, int[] lowerDiagonal, List<List<String>> res){
+        if(col == n){
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                temp.add(new String(board[i]));
+            }
+            res.add(temp);
+            return;
+        }
+        for(int row = 0; row < n; row++){
+            if(leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0){
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+                solve(col + 1, board, n, leftRow, upperDiagonal, lowerDiagonal, res);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
+            }
+        }
+    }
+}
