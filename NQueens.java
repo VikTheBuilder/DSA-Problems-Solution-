15550194a2d0c64a1c3c6110911ffa14b8964d2e
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-09-01 21:55:33 +0530

    Add N-Queens problem solution
    Implement N-Queens solution using backtracking.

--- NQueens.java
+++ NQueens.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) Arrays.fill(row, '.');
+        int[] leftRow = new int[n];
+        int[] lowerDiagonal = new int[2 * n - 1];
+        int[] upperDiagonal = new int[2 * n - 1];
+        solve(0, board, n, leftRow, upperDiagonal, lowerDiagonal, res);
+        return res;
+    }
