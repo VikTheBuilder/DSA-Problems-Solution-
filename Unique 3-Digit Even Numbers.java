f5f15467546b02ed6ba55d2d74ed964803492fdf
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-09-11 11:16:03 +0530

    Implement optimized solution for unique 3-digit even numbers
    Added an optimized solution to calculate unique 3-digit even numbers using a set.

--- Unique 3-Digit Even Numbers.java
+++ Unique 3-Digit Even Numbers.java
@@ -21,3 +21,23 @@ class Solution {
         return c;
     }
 }
+//Optimized Solution--
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> s = new HashSet<>();
+        for(int i=0; i<digits.length; i++){
+            if(digits[i] == 0) continue;
+            for(int j=0; j<digits.length; j++){
+                if(j == i) continue;
+                for(int k=0; k<digits.length; k++){
+                    if(k == i || k == j) continue;
+                    if(digits[k] % 2 == 0){
+                        int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                        s.add(num);
+                    }
+                }
+            }
+        }
+        return s.size();
+    }
+}
