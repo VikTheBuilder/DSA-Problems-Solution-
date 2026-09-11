289919df43116c8d087acdfaea1f97f77fc064b8
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-09-11 11:10:46 +0530

    Implement method to count unique 3-digit even numbers

--- Unique 3-Digit Even Numbers.java
+++ Unique 3-Digit Even Numbers.java
@@ -1,1 +1,23 @@
-
+//Brute Force--
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> s = new HashSet<>();
+        int c=0;
+        for(int i=0; i<digits.length; i++){
+            for(int j=0; j<digits.length; j++){
+                for(int k=0; k<digits.length; k++){
+                    if(digits[i]==0) continue;
+                    if(digits[k] % 2 != 0) continue;
+                    if(i!= j && k!= j && i!= k){
+                        int d = digits[i]*100+ digits[j]*10 + digits[k];
+                        if(!s.contains(d) && d%2==0){
+                            s.add(d);
+                            c++;
+                        }
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
